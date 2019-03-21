2e3f69ff085159df9341bed7e2e7b81074ff516f
Fernando Munoz Mendez <fermm.92@gmail.com>
2019-03-21 10:29:29 +0000

    bins: added n_breaks and round_to parameters

--- R/default_bins.R
+++ R/default_bins.R
@@ -4,25 +4,28 @@
 #'
 #' @param df a dataframe with only numeric columns or a vector
 #' @param midpoint 0 by efault but can be changed
+#' @param round_to round the bins to their ceiling (absolute)by default hundreds (100)
+#' @param n_breaks Number of breaks to each side of the midpoint by default 3
 #' @keywords geometry, coordinates, directions, nb,sb,eb,wb
+#' @importFrom magrittr %>%
 #' @export
 
 
-default_bins <- function(df, midpoint = 0){
+default_bins <- function(df, midpoint = 0, round_to = 100, n_breaks = 3){
   # Gets a dataframe or vector and gives you some good looking bins with a midpoint
   # Get Jenks Natural breaks
   a <- df %>%
     unlist()
   b <- a[a> midpoint]
-  b <- b %>% BAMMtools::getJenksBreaks(k = 3)
+  b <- b %>% BAMMtools::getJenksBreaks(k = n_breaks)
 
   a <- a[a< midpoint]
-  a <- (-(-a %>% BAMMtools::getJenksBreaks(k = 3)))
+  a <- (-(-a %>% BAMMtools::getJenksBreaks(k = n_breaks)))
   # see which ones are negative
   a <- sort(c(a, midpoint ,b))
   negs <- a < 0
-  # apply ceiling for the abs of the vector to the nearest 100
-  a <- ceiling(abs(a)/100000)*100
+  # apply ceiling for the abs of the vector to the nearest round_to
+  a <- ceiling(abs(a)/round_to)*round_to
   # reconvert to negative
   a[negs] <- a[negs] *-1
 
