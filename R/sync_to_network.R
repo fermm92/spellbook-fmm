1ce8fd2cb603be7471dd34ab0f3958caf2e578d4
Fernando Munoz Mendez <fermm.92@gmail.com>
2019-05-08 11:34:27 +0100

    added better sync with network folder

--- R/sync_to_network.R
+++ R/sync_to_network.R
@@ -9,7 +9,23 @@
 #' sync_to_network("path/to.folder")
 
 sync_to_network <- function(net_folder){
+  warning("Deprecated, use robocopy_pfolder() instead")
   command = paste0('XCOPY "." "', net_folder, '" /D /I /S /Y')
+  system(command)
+}
+
+#' Robocopy project folder to network
+#'
+#' Copy project files to another folder and overwrite if the ones you have are newer. Deletes deleted files in the destination folder too. Windows only.
+#'
+#' @param dest_folder destination folder
+#' @keywords copy, sync
+#' @export
+#' @example
+#' robocopy_pfolder("path/to.folder")
+
+robocopy_pfolder <- function(dest_folder){
+  command = paste0('ROBOCOPY "." "', dest_folder, '" /MIR /XD .*')
   system(command)
 }
 
