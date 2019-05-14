9f35bcc79b81c842d54d3e5e787aeac2ad6bb80c
Fernando Munoz Mendez <fermm.92@gmail.com>
2019-05-14 16:23:03 +0100

    add aecom logo on separate file

--- R/map_tools.R
+++ R/map_tools.R
@@ -16,18 +16,4 @@ savemaphtml <- function(m,path){
 }
 
 
-#' Add Aecom Logo
-#'
-#' Adds aecom logo to leaflet maps.
-#'
-#' @param map map for the pipe to work
-#' @param position logo position parameter see addLogo from mapview for options.
-#' @keywords map ,save, htmlwidget
-#' @export
 
-addAecomLogo <- function(map,position = "bottomleft"){
-  mapview::addLogo(map = map,
-                   img = "https://www.aecom.com/wp-content/themes/aecom/images/aecom-logo.svg",
-                   position = position,
-                   url = "https://www.aecom.com")
-}
